8d8c963733a284c35c1f75534274ba8e3d91deea
aswinthottilpalam <aswinp264@gmail.com>
2022-03-13 00:39:19 +0530

    Linked List data structures problem

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace LinkedListPgm
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Linked List Operations");
+            LinkedList list = new LinkedList();
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
+            list.Display();
+        }
+    }
+}

2e7c8009ac89237d50e76aa76d3606fdd45f64a7
aswinthottilpalam <aswinp264@gmail.com>
2022-03-13 01:13:14 +0530

    inserting 30 between 56 and 70

--- Program.cs
+++ Program.cs
@@ -8,9 +8,12 @@ namespace LinkedListPgm
         {
             Console.WriteLine("Linked List Operations");
             LinkedList list = new LinkedList();
-            list.Append(56);
-            list.Append(30);
-            list.Append(70);
+            list.Add(56);
+            list.Add(70);
+            Console.WriteLine("The list before inserting 30 is: ");
+            list.Display();
+            list.InsertAtParticularPosition(2, 30);
+            Console.WriteLine("The list after inserting 30 is: ");
             list.Display();
         }
     }
